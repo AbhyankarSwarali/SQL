15493818334b41c42640888cd61d298de99a3e03
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2025-08-01 16:03:39 +0530

    Add files via upload

---
+++ Payment_Info.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE payment_info;
+USE payment_info;
+
+CREATE TABLE payment (
+	customer_id INT NOT NULL PRIMARY KEY UNIQUE,
+    customer VARCHAR(50),
+    mode VARCHAR (20),
+    city VARCHAR(20)
+);
+
+INSERT INTO payment
+(customer_id, customer, mode, city)
+VALUES
+(101, 'Olivia Barrett', 'Netbanking', 'Portland'),
+(102, 'Ethan Sinclair', 'Credit Card', 'Miami'),
+(103, 'Maya Hemandez', 'Credit Card', 'Seattle'),
+(104, 'Liam Donovan', 'Netbanking', 'Denver'),
+(105, 'Sophia Nguyen', 'Credit Card', 'New Orleans'),
+(106, 'Caleb Foster', 'Debit Card', 'Minneapolis'),
+(107, 'Ava Patel', 'Debit Card', 'Phoenix'),
+(108, 'Lucas Carter', 'Netbanking', 'Boston'),
+(109, 'Isabella Martinez', 'Netbanking', 'Nashville'),
+(110, 'Jackson Brooks', 'Credit Card', 'Boston');
+
+select mode, count(customer_id)
+from payment
+group by mode;
