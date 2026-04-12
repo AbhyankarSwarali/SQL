7554f42515447f9f2975a5114498ba7ca399de1c
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-04-12 07:24:38 +0530

    Add files via upload

---
+++ MySQL/5_HAVING_practice.sql
@@ -0,0 +1,10 @@
+use mydatabase;
+
+-- 5. HAVING
+-- Find the average score for each country considering only customers with a score not equal to 0\
+-- and return only those countries with an average score greater than 430
+select 
+	country, avg(score) as avg_score 
+from customers 
+where score != 0 
+group by country having avg(score) != 0;
