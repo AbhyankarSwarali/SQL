a88261f4495c20962df6ba3f58bf1b56a0bbd683
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-04-14 10:32:19 +0530

    Add files via upload

---
+++ MySQL/16_COMPARISON_OPERATORS.sql
@@ -0,0 +1,17 @@
+-- Retrieve all customers from Germany
+select * from customers where country = 'Germany';
+
+-- Retrieve all customers who are not from Germany
+select * from customers where country != 'Germany';
+
+-- Retrieve all customers with a score greater than 500
+select * from customers where score > 500;
+
+-- Retrieve all customers with a score of 500 or more
+select * from customers where score >= 500;
+
+-- Retrieve all customers with a score less than 500
+select * from customers where score < 500;
+
+-- Retrieve all customers with a score of 500 or less
+select * from customers where score <= 500;
