cca9afd120d1e1dd8d39ed95222e931a6be16b15
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-04-14 10:32:05 +0530

    Add files via upload

---
+++ MySQL/15_DML_DELETE_practice.sql
@@ -0,0 +1,7 @@
+-- Delete all customers with an ID greater than 5
+delete from customers where id > 5;
+select * from customers;
+
+-- Delete all data from the persons table
+truncate table persons;
+select * from persons;
