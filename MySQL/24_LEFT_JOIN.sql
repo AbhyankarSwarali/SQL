29c3aa77689e18e3ac4972cd7ab8ebab377a8a7c
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-04-16 17:54:32 +0530

    Add files via upload

---
+++ MySQL/24_LEFT_JOIN.sql
@@ -0,0 +1,5 @@
+-- Get all customers along with their orders, including those without orders
+select c.id, c.first_name, o.order_id, o.sales
+from customers c
+left join orders o
+on c.id = o.customer_id;
