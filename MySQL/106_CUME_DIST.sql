c93a7c2ea6f4387b70de95ed4246737298db7288
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-05-24 07:08:31 +0530

    Add files via upload

---
+++ MySQL/106_CUME_DIST.sql
@@ -0,0 +1,13 @@
+-- Find the products that fall within the highest 40% of the prices
+use salesdb;
+select 
+	*,
+    concat(DistRank * 100, '%') as DistRankPercent
+    from (
+	select
+		ProductID, 
+		Product, 
+		Price,
+		cume_dist() over(order by Price desc) as DistRank
+	from products) t
+where DistRank <= 0.4;
