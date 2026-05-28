7dddeadeb7d894f81068e0961f5b324608763d42
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2026-05-28 07:02:51 +0530

    Add files via upload

--- MySQL/60_DATE_ADD.sql
+++ MySQL/60_DATE_ADD.sql
@@ -4,7 +4,6 @@ SELECT
     OrderID,
     OrderDate,
     date_add(OrderDate, interval -10 day) as 10_days_earlier,
-    date_add(OrderDate, interval -10 day) as 10_days_earlier,
     DATE_ADD(OrderDate, INTERVAL 2 YEAR) AS 2_Years_Later,
     date_add(OrderDate, interval 3 month) as 3_Months_Later
 FROM
